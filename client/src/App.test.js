0bf373788bf6d8086413ffdf7f6b6f86984e87b2
Luis Daniel Cova <58636486+LuisDanielCova@users.noreply.github.com>
2022-04-13 11:01:37 -0400

    refactor(client): change to html template

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,8 +1,10 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe("It should render the basic layout", () => {
+  test("renders learn react element", () => {
+    render(<App />);
+    const headerElement = screen.getByRole("heading", { name: "Hello World" });
+    expect(headerElement).toBeInTheDocument();
+  });
 });
